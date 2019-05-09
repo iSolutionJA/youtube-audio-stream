b6b9813cb937bb3779ec9f923d759bb4e6abc6c5
Daron McIntosh <mcintoshdaron@gmail.com>
2019-05-08 22:26:29 -0400

    refractor reformat to airbnb style guide add bitrate option add libmp3lame codec

--- index.js
+++ index.js
@@ -1,45 +1,54 @@
 #!/usr/bin/env node
-const ytdl = require('ytdl-core')
-const FFmpeg = require('fluent-ffmpeg')
-const { PassThrough } = require('stream')
-const fs = require('fs')
+const ytdl = require('ytdl-core');
+const FFmpeg = require('fluent-ffmpeg');
+const { PassThrough } = require('stream');
+const fs = require('fs');
 
-if (!module.parent) {
-  const youtubeUrl = process.argv.slice(2)[0]
-  if (!youtubeUrl) throw new TypeError('youtube url not specified')
-  streamify(youtubeUrl).pipe(process.stdout)
-} else {
-  module.exports = streamify
-}
-
-function streamify (uri, opt) {
-  opt = {
+function streamify(uri, opt) {
+  const options = {
     ...opt,
     videoFormat: 'mp4',
     quality: 'lowest',
     audioFormat: 'mp3',
-    filter (format) {
-      return format.container === opt.videoFormat && format.audioEncoding
-    }
-  }
+    filter(format) {
+      return format.container === options.videoFormat && format.audioEncoding;
+    },
+  };
 
-  const video = ytdl(uri, opt)
-  const { file, audioFormat } = opt
-  const stream = file ? fs.createWriteStream(file) : new PassThrough()
-  const ffmpeg = new FFmpeg(video)
+  const video = ytdl(uri, options);
+
+  const { file, audioFormat, bitrate } = options;
+  const stream = file ? fs.createWriteStream(file) : new PassThrough();
+  const audioBitrate = bitrate || 128;
+  const ffmpeg = new FFmpeg(video);
 
   process.nextTick(() => {
-    const output = ffmpeg.format(audioFormat).pipe(stream)
+    const output = ffmpeg
+      .audioCodec('libmp3lame')
+      .audioBitrate(audioBitrate)
+      .format(audioFormat)
+      .pipe(stream);
 
-    ffmpeg.on('error', error => stream.emit('error', error))
-    output.on('error', error => {
-      video.end()
-      stream.emit('error', error)
-    })
-  })
+    ffmpeg.on('error', (error) => {
+      stream.emit('error', error);
+    });
 
-  stream.video = video
-  stream.ffmpeg = ffmpeg
+    output.on('error', (error) => {
+      video.end();
+      stream.emit('error', error);
+    });
+  });
 
-  return stream
+  stream.video = video;
+  stream.ffmpeg = ffmpeg;
+
+  return stream;
+}
+
+if (!module.parent) {
+  const youtubeUrl = process.argv.slice(2)[0];
+  if (!youtubeUrl) throw new TypeError('youtube url not specified');
+  streamify(youtubeUrl).pipe(process.stdout);
+} else {
+  module.exports = streamify;
 }
